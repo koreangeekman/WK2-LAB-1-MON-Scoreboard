9a3ff7bbf15550528e2162d4b6779c33fed47c37
DJ Aaron <koreangeekman@gmail.com>
2023-09-25 15:21:18 -0600

    added functionality - cleaned up CSS

--- script.js
+++ script.js
@@ -2,16 +2,44 @@
 
 let home = 0;
 let away = 0;
+let winPoints = 21;
 
 function addScore(team, points) {
   if (team == 'home') {
-    home += points;
+    if (home + points <= 0) {
+      home = 0;
+    } else {
+      home += points;
+    }
   }
   if (team == 'away') {
-    away += points;
+    if (away + points <= 0) {
+      away = 0;
+    } else {
+      away += points;
+    }
   }
   // console.log('addScore', team, points)
   draw();
+  checkWin();
+}
+
+function checkWin() {
+  if (home >= 21) {
+    declareWin('Home');
+  }
+  if (away >= 21) {
+    declareWin('Away');
+  }
+  console.log('score check')
+  console.log('home', home);
+  console.log('away', away);
+}
+
+function declareWin(team) {
+  console.log(`${team} team wins!`);
+  window.alert(`${team} team wins!`);
+  resetBoard();
 }
 
 function resetBoard() {
