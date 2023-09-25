5d798f9891fe1f3651a310790b8eaa9e46b40d4b
DJ Aaron <koreangeekman@gmail.com>
2023-09-25 14:42:05 -0600

    functional

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("test testt")
+// Scoreboard JS
+
+let home = 0;
+let away = 0;
+
+function addScore(team, points) {
+  if (team == 'home') {
+    home += points;
+  }
+  if (team == 'away') {
+    away += points;
+  }
+  console.log('addScore', team, points)
+  draw();
+}
+
+function resetBoard() {
+  home = 0;
+  away = 0;
+  draw();
+  console.log('attempted scores reset')
+}
+
+function draw() {
+  document.getElementById('homeScore').innerText = home;
+  document.getElementById('awayScore').innerText = away;
+  console.log('attempted scores draw')
+}
